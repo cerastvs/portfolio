9b265411bbeff46d259ac36eadfc40d9c6771ecd
Lieu Rik <yuriksoliman@gmail.com>
2025-06-19 09:14:45 +0800

    adds animation to buttons and skills container

--- src/components/ValueCard.jsx
+++ src/components/ValueCard.jsx
@@ -1,8 +1,16 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function ValueCard({ headding, subHeadding, last }) {
   return (
-    <div className={`skill${last ? " last" : ""}`}>
+    <motion.div
+      className={`skill${last ? " last" : ""}`}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3 }}
+    >
       <h3 className="skill-heading">{headding}</h3>
       <p className="skill-sub-heading">{subHeadding}</p>
-    </div>
+    </motion.div>
   );
 }
